51e4ac7051dd43ab62a378941ae821e1cc41daa7
PelleLindroth <pellelindroth@gmail.com>
2021-10-28 17:14:12 +0200

    Console log unused vars

--- problems/medium/digits.js
+++ problems/medium/digits.js
@@ -3,12 +3,13 @@
 // So for 2342 it should print [2,3,4,2].
 
 export const digits = (n) => {
+	console.log(n)
 	// Your code here...
 	return []
 }
 
 const tests = [
-	[digits, [2342], [2,3,4,2]],
+	[digits, [2342], [2, 3, 4, 2]],
 	[digits, [123456], [1, 2, 3, 4, 5, 6]],
 	[digits, [1], [1]],
 	[digits, [981092830912], [9, 8, 1, 0, 9, 2, 8, 3, 0, 9, 1, 2]],
