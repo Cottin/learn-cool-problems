2024498b0a7ac2d6af90b3d255ae77d003b0643d
PelleLindroth <pellelindroth@gmail.com>
2021-10-28 12:49:32 +0200

    Solve easy/sum1toN and add test

---
+++ problems/easy/test_sum1toN.js
@@ -0,0 +1,19 @@
+import assert from 'assert'
+import { sum1toN } from './sum1toN.js'
+
+describe("sum1toN", () => {
+  it('returns the sum of 1,...,n', () => {
+    const response = sum1toN(80)
+    assert.equal(response, 3240)
+  })
+
+  it('returns -1 for 0', () => {
+    const response = sum1toN(0)
+    assert.equal(response, -1)
+  })
+
+  it('returns -1 for negative numbers', () => {
+    const response = sum1toN(-34)
+    assert.equal(response, -1)
+  })
+})
