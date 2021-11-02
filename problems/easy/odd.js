2cffd98c88c33cef0e5c7ffab4e5e79ee43391e8
hanna <hanna.aspblad@iths.se>
2021-11-02 12:34:33 +0100

    solved odd

--- problems/easy/odd.js
+++ problems/easy/odd.js
@@ -1,18 +1,19 @@
-
 // Write a function that returns the odd numbers in a given list.
 // eg. [1, 2, 3, 4, 5] -> [1, 3, 5]
 
 export const odd = (xs) => {
-	// Your code here...
-	return []
-}
+  const list = [];
+  for (let num of xs) {
+    num % 2 !== 0 ? list.push(num) : "";
+  }
+  return list;
+};
 
 const tests = [
-	[odd, [[1, 2, 3, 4, 5]], [1, 3, 5]],
-	[odd, [[2, 4, 6]], []],
-	[odd, [[9, 13, 17, 99]], [9, 13, 17, 99]],
-	[odd, [[0, 2, 5, 182391, 283182372]], [5, 182391]],
-]
+  [odd, [[1, 2, 3, 4, 5]], [1, 3, 5]],
+  [odd, [[2, 4, 6]], []],
+  [odd, [[9, 13, 17, 99]], [9, 13, 17, 99]],
+  [odd, [[0, 2, 5, 182391, 283182372]], [5, 182391]],
+];
 
-export default tests
-
+export default tests;
